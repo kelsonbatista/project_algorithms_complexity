1933c2807f8c377a34a29da4af303ab33c4e01f3
kelsonbatista <kelsonbatista@gmail.com>
2022-10-10 18:38:23 -0300

    feat: requisito2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    n = list(word)
+    if word == '': return False
+    if n[low_index] != n[high_index]: return False
+    if low_index >= high_index: return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# word = 'araraaaaa'
+# print(is_palindrome_recursive(word, 0, len(word) - 1))
