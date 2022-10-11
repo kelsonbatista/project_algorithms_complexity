f23ae469d140169b4a3130ec17180b25113e45e1
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 11:59:44 -0300

    feat: requisito4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) < 2: return False
+    if type(nums) != list: return False
+    for i in range(0, len(nums) - 1):
+        if type(nums[i]) != int: return False
+        if nums[i] < 0: return False
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]: return nums[i]
+    return False
+    
+
+# print(find_duplicate([1, 3, 4, 2, 2]))
