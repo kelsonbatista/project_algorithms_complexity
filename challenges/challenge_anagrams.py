979a866b03b390ae8019b0973b91d1e8d9c464ff
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 17:23:32 -0300

    refactor: req3 removed lint issues

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -11,11 +11,12 @@ def is_anagram(first_string, second_string):
     s1 = list(first_string.lower())
     s2 = list(second_string.lower())
     response = validate(first_string, second_string)
-    if response is False: return False
+    if response is False:
+        return False
     for i in s1:
         if i not in s2 or s1.count(i) != s2.count(i):
             return False
-    for i in s2:
-        if i not in s1 or s1.count(i) != s2.count(i):
-            return False  
+    # for i in s2:
+    #     if i not in s1 or s1.count(i) != s2.count(i):
+    #         return False
     return True
