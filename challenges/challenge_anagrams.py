a60a64c1ffe5da2f7f701de1dfb3663afb29d867
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 12:52:25 -0300

    feat: requisito3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+def validate(first_string, second_string):
+    if first_string == '':
+        return False
+    if second_string == '':
+        return False
+    if len(first_string) != len(second_string):
+        return False
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    s1 = list(first_string.lower())
+    s2 = list(second_string.lower())
+    validate(first_string, second_string)
+    for i in s1:
+        if i not in s2 or s1.count(i) != s2.count(i):
+            return False
+    for i in s2:
+        if i not in s1 or s1.count(i) != s2.count(i):
+            return False
+    return True
