b430f52a5abca3ff914db0bf143e0c9731004454
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 17:17:12 -0300

    refactor: requisito4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -3,15 +3,37 @@ def validate(nums):
         return False
 
 
+def sort_nums(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        for item in range(0, n - 1 - i):
+            if nums[item] > nums[item + 1]:
+                current = nums[item]
+                nums[item] = nums[item + 1]
+                nums[item + 1] = current
+    return nums
+
+
 def find_duplicate(nums):
     validate(nums)
-    for i in range(0, len(nums) - 1):
+    ordered = sort_nums(nums)
+    for i in range(0, len(ordered) - 1):
         if type(nums[i]) != int or nums[i] < 0:
             return False
-        for j in range(i + 1, len(nums)):
-            if nums[i] == nums[j]:
-                return nums[i]
+        if ordered[i] == ordered[i + 1]:
+            return ordered[i]
     return False
+
+
+# def find_duplicate(nums):
+#     validate(nums)
+#     for i in range(0, len(nums) - 1):
+#         if type(nums[i]) != int or nums[i] < 0:
+#             return False
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] == nums[j]:
+#                 return nums[i]
+#     return False
 
 
 # print(find_duplicate([1, 3, 4, 2, 2]))
