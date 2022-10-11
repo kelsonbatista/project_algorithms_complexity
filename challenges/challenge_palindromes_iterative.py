8205da4e0fceee5b01c293a80854ca335b3fc0ff
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 11:20:51 -0300

    feat: requisito5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    n = list(word)
+    size = len(word)
+    if word == '': return False
+    for i in range(size - 1):
+        if n[i] != n[size - i - 1]: return False
+        if i >= size - i: return True
+    return True
+
+
+# word = 'revivir'
+# print(is_palindrome_iterative(word))
