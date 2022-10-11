9b91fc8a69a352f98d6b2a3a77e633d7e5246a77
kelsonbatista <kelsonbatista@gmail.com>
2022-10-11 17:18:01 -0300

    refactor: requisito3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -10,11 +10,12 @@ def validate(first_string, second_string):
 def is_anagram(first_string, second_string):
     s1 = list(first_string.lower())
     s2 = list(second_string.lower())
-    validate(first_string, second_string)
+    response = validate(first_string, second_string)
+    if response is False: return False
     for i in s1:
         if i not in s2 or s1.count(i) != s2.count(i):
             return False
     for i in s2:
         if i not in s1 or s1.count(i) != s2.count(i):
-            return False
+            return False  
     return True
